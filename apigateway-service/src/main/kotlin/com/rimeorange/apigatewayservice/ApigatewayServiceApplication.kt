92d7deab7b97251067f3a2c1964696b506bf1c6f
rimeorange <simwb@hanmail.net>
2025-06-08 12:33:32 +0900

    feat: apigateway init commit

---
+++ apigateway-service/src/main/kotlin/com/rimeorange/apigatewayservice/ApigatewayServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.rimeorange.apigatewayservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApigatewayServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApigatewayServiceApplication>(*args)
+}
